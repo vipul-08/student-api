a1f95637eb9f9ec30f3d0dd89b98cf3220935a80
vipul.s <vipul.s@media.net>
2021-02-22 20:44:25 +0530

    Added local env support

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/vipul-08/student-api/app"
 	"github.com/vipul-08/student-api/db"
+	"os"
 )
 
 func main() {
+	if os.Getenv("ENVIRONMENT") != "prod" {
+		err := godotenv.Load()
+		if err != nil {
+			panic("Unable to load .env file")
+		}
+	}
 	db.ConnectDb()
 	app.StartRoutes()
 }
